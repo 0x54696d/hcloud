d40f0b6abf9af0b6b6b0dfa39dce733523e859ff
0x54696d <mail@timjaeger.de>
2024-07-07 15:19:51 +0200

    ✨ Add API client

---
+++ lib/src/api_client.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+
+import 'models/ssh_key_response.dart';
+
+part 'api_client.g.dart';
+
+@RestApi(baseUrl: 'https://api.hetzner.cloud/v1')
+abstract class ApiClient {
+  factory ApiClient(Dio dio) = _ApiClient;
+
+  @GET('/ssh_keys')
+  Future<SSHKeysResponse> getSSHKeys(@Queries() Map<String, dynamic>? queries);
+
+  @GET('/ssh_keys/{id}')
+  Future<SSHKeyResponse> getSSHKey(@Path('id') int id);
+
+  @POST('/ssh_keys')
+  Future<SSHKeyResponse> createSSHKey(@Body() Map<String, dynamic> body);
+
+  @DELETE('/ssh_keys/{id}')
+  Future<void> deleteSSHKey(@Path('id') int id);
+
+  @PUT('/ssh_keys/{id}')
+  Future<SSHKeyResponse> updateSSHKey(
+      @Path('id') int id, @Body() Map<String, dynamic> body);
+}
---
+++ lib/src/api_client.g.dart
@@ -0,0 +1,191 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_client.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
+
+class _ApiClient implements ApiClient {
+  _ApiClient(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'https://api.hetzner.cloud/v1';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<SSHKeysResponse> getSSHKeys(Map<String, dynamic>? queries) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    queryParameters.addAll(queries ?? <String, dynamic>{});
+    queryParameters.removeWhere((k, v) => v == null);
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<SSHKeysResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/ssh_keys',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final _value = SSHKeysResponse.fromJson(_result.data!);
+    return _value;
+  }
+
+  @override
+  Future<SSHKeyResponse> getSSHKey(int id) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<SSHKeyResponse>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/ssh_keys/${id}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final _value = SSHKeyResponse.fromJson(_result.data!);
+    return _value;
+  }
+
+  @override
+  Future<SSHKeyResponse> createSSHKey(Map<String, dynamic> body) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(body);
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<SSHKeyResponse>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/ssh_keys',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final _value = SSHKeyResponse.fromJson(_result.data!);
+    return _value;
+  }
+
+  @override
+  Future<void> deleteSSHKey(int id) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    await _dio.fetch<void>(_setStreamType<void>(Options(
+      method: 'DELETE',
+      headers: _headers,
+      extra: _extra,
+    )
+        .compose(
+          _dio.options,
+          '/ssh_keys/${id}',
+          queryParameters: queryParameters,
+          data: _data,
+        )
+        .copyWith(
+            baseUrl: _combineBaseUrls(
+          _dio.options.baseUrl,
+          baseUrl,
+        ))));
+  }
+
+  @override
+  Future<SSHKeyResponse> updateSSHKey(
+    int id,
+    Map<String, dynamic> body,
+  ) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(body);
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<SSHKeyResponse>(Options(
+      method: 'PUT',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/ssh_keys/${id}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final _value = SSHKeyResponse.fromJson(_result.data!);
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
